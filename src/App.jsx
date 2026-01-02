20b5f68853d2ef3f96edabc990970e0dce40fa47
Akil-Hub <akilanjummahi@gmail.com>
2026-01-02 14:09:25 +0600

    dones

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-6xl bg-red-600 ">welcome to react Universal template.</h1>
+    </>
+  );
+};
+
+export default App;
